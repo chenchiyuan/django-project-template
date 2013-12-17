7ddbd9504701038b2a91cf824d41650dcf66527c
chenchiyuan <chenchiyuan03@gmail.com>
2013-12-17 17:54:43 +0800

    config化

---
+++ README.md
@@ -0,0 +1,8 @@
+### 项目模板
+
+最近做的django项目比较多，考察了很多django项目布局，发觉[Heroku的推荐](http://www.deploydjango.com/django_project_structure/index.html#the-basics-a-default-django-project)最符合我心中所想。
+
+
+### 项目布局
+
+边写边更新吧
--- project_name/settings.py
+++ project_name/settings.py
@@ -1,20 +1,27 @@
-# Django settings for template project.
+# Django settings for movie project.
+import os
+import json
+from config import config
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+DEBUG = config.getboolean("django", "debug")
+TEMPLATE_DEBUG = config.getboolean("django", "template_dubug")
+
+ALLOWED_HOSTS = json.loads(config.get("django", "allowed_hosts"))
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Shadow', 'chenchiyuan03@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '{project_name}',                      # Or path to database file if using sqlite3.
+        'USER': 'shadow',                      # Not used with sqlite3.
+        'PASSWORD': '900303',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -24,11 +31,11 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 SITE_ID = 1
 
@@ -77,14 +84,11 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = 'dly31d$+kks@z_!jpie*zw3t=06_as+z(*q8&amp;j0e7p30-euon-'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,11 +97,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'template.urls'
+ROOT_URLCONF = 'project_name.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'template.wsgi.application'
@@ -149,3 +151,5 @@ LOGGING = {
         },
     }
 }
+
+INTERNAL_IPS = ['127.0.0.1']
